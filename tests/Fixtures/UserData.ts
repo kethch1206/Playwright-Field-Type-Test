dd0f4047a26c98fdaa86629f902044c3e2fb1831
kethch1206 <kethch1206@hotmail.com>
2025-07-14 14:55:31 -0400

    QA | add email, phone, address testing

--- tests/Fixtures/UserData.ts
+++ tests/Fixtures/UserData.ts
@@ -1,1 +1,20 @@
-export const userList = [{ name: "Alice" }, { name: "123" }, { name: "@#$%" }];
+export const userList = [
+  {
+    name: "Alice",
+    email: "alice.doe@gmail.com",
+    phone: "1234567890",
+    address: "8444 Lundy's Ln, Niagara Falls, ON L2H 1H4",
+  },
+  {
+    name: "123",
+    email: "123.doe@gmail.com",
+    phone: "",
+    address: "443 Main St, Niagara Falls, NY 14301, United States",
+  },
+  {
+    name: "@#$%",
+    email: "",
+    phone: "",
+    address: "4001 Weaver Rd, Niagara Falls, ON L2E 6S6",
+  },
+];
